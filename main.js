61e30789bc5d3122103dff7485267008a1ec9cb3
Hussein6685 <husseinail6685@gmail.com>
2023-09-24 19:11:27 +0300

    commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// letters
+const letters = "abcdefghijklmnopqrstuvwxyz123456789ابت";
+//  get array from  letters
+let lettersArray = Array.from(letters);
+// select letters container
+let lettersContainer = document.querySelector(".letters");
+// generate letters
+lettersArray.forEach(letter => {
+    //create span
+    let span = document.createElement("span");
+    // create letter text node
+    let theLetter = document.createTextNode(letter);
+    // append the letter to span
+    span.appendChild(theLetter);
+    // add class on span
+    span.className = 'letter-box';
+    // append span to the letters container
+    lettersContainer.appendChild(span);
+});
